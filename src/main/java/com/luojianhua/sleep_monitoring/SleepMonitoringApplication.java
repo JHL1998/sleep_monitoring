743438e866bd868ceadd4ab9ef6cbbfa060192a9
JianhuaLuo <913955421@qq.com>
2020-06-23 10:30:46 +0800

    first-commt

---
+++ src/main/java/com/luojianhua/sleep_monitoring/SleepMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.luojianhua.sleep_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luojianhua/sleep_monitoring/SleepMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luojianhua.sleep_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
